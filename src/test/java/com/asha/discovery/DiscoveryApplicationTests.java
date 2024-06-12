43122e780be5421c1afbb645d868a8ced503a1de
Asha NAmbissan <asha.nambissan@gmail.com>
2024-06-12 18:22:31 +0530

    creating initial project

---
+++ src/test/java/com/asha/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asha.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
